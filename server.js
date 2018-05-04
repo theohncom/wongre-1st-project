4e84bd528ebbf483bdedf2a641d237b1ce2d29c3
Sophon Mongkolluksamee <theohncom@gmail.com>
2018-05-04 15:27:08 +0700

    this is first draft of out frontend

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+var pathView =  __dirname+'/views/';
+
+//app.use('/js',express.static(__dirname+'/js'));
+app.use('/js',express.static(__dirname+'/node_modules/bootstrap/dist/js'));
+app.use('/js',express.static(__dirname+'/node_modules/jquery/dist'));
+app.use('/js',express.static(__dirname+'/node_modules/popper.js/dist/umd'));
+app.use('/css',express.static(__dirname+'/node_modules/bootstrap/dist/css'));
+app.use('/css',express.static(__dirname+'/customcss'));
+
+app.get('/',function(req,res){
+    //res.sendFile(pathView+'index.html');
+    res.sendFile(pathView + "index.html");
+})
+
+app.listen(3000,function(){
+    console.log('Server running on port 3000')
+})
